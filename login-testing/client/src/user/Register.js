2a126e2ff7c9bd6078123931ce9ed8e09f46fb66
gergof2 <heroghost20001024@gmail.com>
2021-10-04 08:42:23 +0200

    update: db password, and register page form (waiting for tests)

--- login-testing/client/src/user/Register.js
+++ login-testing/client/src/user/Register.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 
+
+
+
 class Register extends Component {
   state = { user_name: '', email: '', password: '' };
 
-  handleInputChange = event => {
-    const { value, name } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
+    handleInputChange = event => {
+      const { value, name } = event.target;
+      this.setState({
+        [name]: value
+      });
+    };
+
+  
 
   onSubmit = event => {
     event.preventDefault();
@@ -25,41 +30,54 @@ class Register extends Component {
 
   render() {
     return (
-      <form  classname="mt-5" onSubmit={this.onSubmit} noValidate>
-        <h1 className="text-left mt-5">Register</h1>        
-          <div class="modal-body">
-          <input
-            type="text"
-            name="user_name"
-            placeholder="Enter user name"
-            value={this.state.user_name}
-            onChange={this.handleInputChange}
-          />
-          </div>
-          <div class="modal-body">
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-          />
-          </div>
-          <div class="modal-body">
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-          />
-          </div>
-          
-          <input class="modal-body" type="submit" value="Register"/>
-          
+      <form className="mt-5" onSubmit={this.onSubmit}>
+        <h1>Registration</h1>
+        <div>
+          <input type="text" name="user_name" 
+          placeholder="User name" value={user_name} onChange={this.handleInputChange}></input>
+          <input type="email" name="email" 
+          placeholder="Email" value={email} onChange={this.handleInputChange}></input>
+          <input tpye="password" name="password" 
+          placeholder="Password" value={password} onChange={this.handleInputChange}></input>
+          <btn className="btn btn-primary" type="submit">Regist</btn>
+        </div>
       </form>
     );
   }
 }
+
+//<form  classname="mt-5" onSubmit={this.onSubmit} noValidate>
+//        <h1 className="text-left mt-5">Register</h1>        
+//          <div class="modal-body">
+//          <input
+//            type="text"
+//            name="user_name"
+//            placeholder="Enter user name"
+//            value={this.state.user_name}
+//            onChange={this.handleInputChange}
+//          />
+//          </div>
+//          <div class="modal-body">
+//          <input
+//            type="email"
+//            name="email"
+//            placeholder="Enter email"
+//            value={this.state.email}
+//            onChange={this.handleInputChange}
+//          />
+//          </div>
+//          <div class="modal-body">
+//          <input
+//            type="password"
+//            name="password"
+//            placeholder="Enter password"
+//            value={this.state.password}
+//            onChange={this.handleInputChange}
+//          />
+//          </div>
+//          
+//          <input class="modal-body" type="submit" value="Register"/>
+//          
+//      </form>
 
 export default Register;
--- login-testing/server/db.js
+++ login-testing/server/db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "garfild222",
+    password: "password",
     host: "localhost",
     port: 5432,
     database: "test_login"
